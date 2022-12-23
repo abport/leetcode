37dc656ba2a0b342f69a5388a71f1e0b4f5c07f6
Amin Beheshti <81535210+abport@users.noreply.github.com>
2022-12-23 20:38:47 +0330

    Update fibonacci_number.py

--- 509. Fibonacci Number/fibonacci_number.py
+++ 509. Fibonacci Number/fibonacci_number.py
@@ -1,30 +1,17 @@
-def isValid(s: str) -> bool:
-    # Create an empty stack to keep track of open brackets
-    stack = []
+def fibonacci(n):
+    # Base case: handle input of 0
+    if n == 0:
+        return 0
     
-    # Iterate through each character in the string
-    for c in s:
-        # If the character is an open bracket, push it onto the stack
-        if c in ('(', '[', '{'):
-            stack.append(c)
-        # If the character is a close bracket, 
-        # pop the top element from the stack and check if it is the corresponding open bracket
-        elif c in (')', ']', '}'):
-            # If the stack is empty, then there is no corresponding open bracket
-            # so the input string is not valid
-            if not stack:
-                return False
-            # Check if the top element on the stack is the corresponding open bracket
-            # If it is not, then the input string is not valid
-            if c == ')' and stack[-1] != '(':
-                return False
-            if c == ']' and stack[-1] != '[':
-                return False
-            if c == '}' and stack[-1] != '{':
-                return False
-            # If the top element on the stack is the corresponding open bracket,
-            # remove it from the stack
-            stack.pop()
-    # If the stack is empty at the end, then all open brackets have been closed
-    # so the input string is valid. Otherwise, it is not valid.
-    return not stack
+    # Create a list to store the results of subproblems
+    memo = [0] * (n+1)
+    
+    # Base case: F(1) = 1
+    memo[1] = 1
+    
+    # Use a loop to calculate the remaining values
+    for i in range(2, n+1):
+        memo[i] = memo[i-1] + memo[i-2]
+    
+    # Return the result
+    return memo[n]
